a5e056e2f3d8887351d34b7abd8cc8b528e95efb
David Čadež <david.cadez89@gmail.com>
2023-11-05 21:45:15 +0000

    reduced delay in SplashScreenActivity

--- app/src/main/java/com/example/expensemate/SplashScreenActivity.java
+++ app/src/main/java/com/example/expensemate/SplashScreenActivity.java
@@ -21,6 +21,6 @@ public class SplashScreenActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 3000);
+        }, 2000);
     }
 }
