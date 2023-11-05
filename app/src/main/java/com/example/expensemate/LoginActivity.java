a1ec4827f35912c862e3cb52d8e8f6cd10732579
David Čadež <david.cadez89@gmail.com>
2023-11-05 21:44:21 +0000

    added navigation between Login and Register activity

--- app/src/main/java/com/example/expensemate/LoginActivity.java
+++ app/src/main/java/com/example/expensemate/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.expensemate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView txtVRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtVRegister = findViewById(R.id.txtVRegister);
+        txtVRegister.setOnClickListener(view -> {
+            // Open Register Activity
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
     }
+
+
 }
--- app/src/main/java/com/example/expensemate/RegisterActivity.java
+++ app/src/main/java/com/example/expensemate/RegisterActivity.java
@@ -2,13 +2,23 @@ package com.example.expensemate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    TextView txtVLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        txtVLogin = findViewById(R.id.txtVLogin);
+        txtVLogin.setOnClickListener(view -> {
+            // Open Login Activity
+            finish();
+        });
     }
 }
