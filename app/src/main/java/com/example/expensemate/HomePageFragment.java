e9f6b95f4c98e8d0d131c74c996c6853f4671edf
David Čadež <david.cadez89@gmail.com>
2023-11-15 16:29:39 +0000

    added navigation from fragment home page to transaction activity

--- app/src/main/java/com/example/expensemate/HomePageFragment.java
+++ app/src/main/java/com/example/expensemate/HomePageFragment.java
@@ -1,12 +1,13 @@
 package com.example.expensemate;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
 
 
 public class HomePageFragment extends Fragment {
@@ -15,10 +16,25 @@ public class HomePageFragment extends Fragment {
         // Required empty public constructor
     }
 
+    // DECLARING COMPONENTS
+    ImageView imgVAddTransaction;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
+
+        // INITIALIZING COMPONENTS
+        imgVAddTransaction = view.findViewById(R.id.imgVAddTransaction);
+
+        // SETTING ON CLICK LISTENERS
+        imgVAddTransaction.setOnClickListener(v -> {
+            // Open Transaction Activity
+            Intent intentTransactionActivity = new Intent(getActivity(), TransactionActivity.class);
+            startActivity(intentTransactionActivity);
+        });
+
+
+        return view;
     }
 }
