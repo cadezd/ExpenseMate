06c4f2f99f4e149f44f714a5356743c782e609c0
David Čadež <david.cadez89@gmail.com>
2023-11-15 11:48:32 +0000

    replaced async tasks with executors

--- app/src/main/java/com/example/expensemate/databse/repository/UserRepository.java
+++ app/src/main/java/com/example/expensemate/databse/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package com.example.expensemate.databse.repository;
 
 import android.app.Application;
-import android.os.AsyncTask;
-import android.view.View;
 
 import androidx.lifecycle.LiveData;
 
@@ -10,8 +8,8 @@ import com.example.expensemate.databse.connection.MyDatabase;
 import com.example.expensemate.databse.dao.Dao;
 import com.example.expensemate.databse.entities.User;
 
-import java.util.List;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class UserRepository {
@@ -23,15 +21,24 @@ public class UserRepository {
     }
 
     public void insertUser(User user) {
-        new InsertUserAsyncTask(dao).execute(user);
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        service.execute(() -> {
+            dao.insertUser(user);
+        });
     }
 
     public void deleteUser(User user) {
-        new DeleteUserAsyncTask(dao).execute(user);
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        service.execute(() -> {
+            dao.deleteUser(user);
+        });
     }
 
     public void updateUser(User user) {
-        new UpdateUserAsyncTask(dao).execute(user);
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        service.execute(() -> {
+            dao.updateUser(user);
+        });
     }
 
     public LiveData<User> findUserByUsernameAndPassword(String username, String password) {
@@ -41,49 +48,6 @@ public class UserRepository {
     public Boolean isUsernameTaken(String username) {
         return dao.isUsernameTaken(username);
     }
-
-    private static class InsertUserAsyncTask extends android.os.AsyncTask<User, Void, Void> {
-        private Dao dao;
-
-        private InsertUserAsyncTask(Dao dao) {
-            this.dao = dao;
-        }
-
-        @Override
-        protected Void doInBackground(User... users) {
-            dao.insertUser(users[0]);
-            return null;
-        }
-    }
-
-    private static class DeleteUserAsyncTask extends android.os.AsyncTask<User, Void, Void> {
-        private Dao dao;
-
-        private DeleteUserAsyncTask(Dao dao) {
-            this.dao = dao;
-        }
-
-        @Override
-        protected Void doInBackground(User... users) {
-            dao.deleteUser(users[0]);
-            return null;
-        }
-    }
-
-    private static class UpdateUserAsyncTask extends android.os.AsyncTask<User, Void, Void> {
-        private Dao dao;
-
-        private UpdateUserAsyncTask(Dao dao) {
-            this.dao = dao;
-        }
-
-        @Override
-        protected Void doInBackground(User... users) {
-            dao.updateUser(users[0]);
-            return null;
-        }
-    }
-
 }
 
 
