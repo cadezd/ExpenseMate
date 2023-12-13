f66dfc5d019af0ad9fa89c422d3702b18d208d16
David Čadež <david.cadez89@gmail.com>
2023-12-13 11:36:29 +0000

    Added functionality to gather data from the user (transaction description, amount, date and image).

--- app/src/main/java/com/example/expensemate/TransactionActivity.java
+++ app/src/main/java/com/example/expensemate/TransactionActivity.java
@@ -2,7 +2,10 @@ package com.example.expensemate;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.util.Log;
@@ -12,14 +15,19 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.expensemate.constants.Constants;
-import com.example.expensemate.databse.entities.UserTransaction;
+import com.example.expensemate.databse.converters.ImageConverter;
 import com.example.expensemate.util.DecimalFilter;
 import com.github.dhaval2404.imagepicker.ImagePicker;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -31,6 +39,10 @@ public class TransactionActivity extends AppCompatActivity {
 
     Button btnSave, btnAddImage;
 
+    // DECLARING VARIABLES
+    int id = -1;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,8 +61,26 @@ public class TransactionActivity extends AppCompatActivity {
 
         // Get Intent
         Intent intent = getIntent();
-        if (!intent.hasExtra(Constants.NEW_TRANSACTION_TAG)) {
-            // TODO: set the data to the inputs
+        // Set the values to the inputs tu update transaction
+        if (intent.hasExtra(Constants.TRANSACTION_ID_TAG)) {
+            // Get transaction data from intent
+            id = intent.getIntExtra(Constants.TRANSACTION_ID_TAG, -1);
+            String description = intent.getStringExtra(Constants.TRANSACTION_DESCRIPTION_TAG);
+            double amount = intent.getDoubleExtra(Constants.TRANSACTION_AMOUNT_TAG, 0);
+            Date date = (Date) intent.getSerializableExtra(Constants.TRANSACTION_DATE_TAG);
+            byte[] imageInByte = intent.getByteArrayExtra(Constants.TRANSACTION_IMAGE_TAG);
+
+            // Set transaction data to inputs
+            txtInDescription.setText(description);
+            txtInAmount.setText(String.valueOf(amount));
+            txtInDate.setText(parseDateToString(date));
+
+            if (imageInByte != null) {
+                // SOURCE START: https://stackoverflow.com/questions/13854742/byte-array-of-image-into-imageview
+                imgVPicture.setImageBitmap(ImageConverter.toBitmap(imageInByte));
+                // SOURCE END: https://stackoverflow.com/questions/13854742/byte-array-of-image-into-imageview
+                btnAddImage.setText("Remove Image");
+            }
         }
 
         // SETTING THE FILTERS
@@ -88,10 +118,11 @@ public class TransactionActivity extends AppCompatActivity {
         // SOURCE END: https://www.geeksforgeeks.org/datepicker-in-android/
 
         btnAddImage.setOnClickListener(v -> {
+            // Add or remove image
             if (imgVPicture.getDrawable() == null) {
                 ImagePicker.Companion.with(this)
-                        .compress(1024)
-                        .maxResultSize(1080, 1080)
+                        .compress(512)
+                        .maxResultSize(720, 720)
                         .start();
             } else {
                 imgVPicture.setImageDrawable(null);
@@ -100,30 +131,39 @@ public class TransactionActivity extends AppCompatActivity {
         });
 
         btnSave.setOnClickListener(v -> {
-            Log.d("TEST", "gathering data");
-            // Collect data from inputs and send it to the main activity
-            String description = txtInDescription.getText().toString().trim();
-            double amount = Double.parseDouble(txtInAmount.getText().toString().trim());
-            Date date = null;
-            byte[] imageInByte = null;
+            try {
+                // Check if all the fields are filled except image
+                if (txtInDescription.getText().toString().trim().isEmpty() || txtInAmount.getText().toString().trim().isEmpty() || txtInDate.getText().toString().trim().isEmpty())
+                    throw new Exception("Please fill all the fields.");
 
-            // TODO: get image from image view and date from date picker
+                // Collect data from inputs and send it to the main activity
+                String description = txtInDescription.getText().toString().trim();
+                double amount = Double.parseDouble(txtInAmount.getText().toString().trim());
+                Date date = parseStringToDate(txtInDate.getText().toString().trim());
+                Bitmap bitmap = null;
+                if (imgVPicture.getDrawable() != null)
+                    bitmap = ((BitmapDrawable) imgVPicture.getDrawable()).getBitmap();
 
+                // Send transaction data to main activity
+                Intent data = new Intent();
+                data.putExtra(Constants.TRANSACTION_ID_TAG, id);
+                data.putExtra(Constants.TRANSACTION_DESCRIPTION_TAG, description);
+                data.putExtra(Constants.TRANSACTION_AMOUNT_TAG, amount);
+                data.putExtra(Constants.TRANSACTION_DATE_TAG, date);
+                data.putExtra(Constants.TRANSACTION_IMAGE_TAG, ImageConverter.fromBitmap(bitmap));
 
-            // Create new transaction
-            UserTransaction transaction = new UserTransaction(1, description, amount, date, imageInByte);
-
-            // Send transaction to main activity
-            Log.d("TEST", "sending data to main activity");
-            Intent data = new Intent();
-            data.putExtra(Constants.TRANSACTION_TAG, transaction);
-            setResult(RESULT_OK, data);
-            finish();
+                // Set result and finish activity
+                setResult(RESULT_OK, data);
+                finish();
+            } catch (Exception e) {
+                displayErrorMessage("Error", "Error: " + e.getMessage());
+            }
         });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // This method is called when image is selected from gallery or camera
         super.onActivityResult(requestCode, resultCode, data);
 
         if (resultCode != RESULT_OK)
@@ -146,5 +186,31 @@ public class TransactionActivity extends AppCompatActivity {
         builder.setMessage(message);
         builder.setPositiveButton("OK", null);
         builder.show();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private Date parseStringToDate(String date) {
+        try {
+            // Convert string to date
+            // SOURCE START: https://stackoverflow.com/questions/22929237/convert-java-time-localdate-into-java-util-date-type
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            LocalDate localDate = LocalDate.parse(date, formatter);
+            return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+            // SOURCE END: https://stackoverflow.com/questions/22929237/convert-java-time-localdate-into-java-util-date-type
+        } catch (Exception e) {
+            Log.e("Error", "Error: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private String parseDateToString(Date date) {
+        try {
+            // Convert date to string
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+            return dateFormat.format(date);
+        } catch (Exception e) {
+            Log.e("Error", "Error: " + e.getMessage());
+            return null;
+        }
     }
 }
